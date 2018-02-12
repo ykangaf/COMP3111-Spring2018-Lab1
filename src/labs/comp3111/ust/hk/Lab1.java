d356974e37ce7dbad9b2d50d5b5bb7c2b37fcb2f
yanmingkang <yanmingkang@10.89.207.57>
2018-02-12 13:04:57 +0800

    Added a congratulation message.

--- src/labs/comp3111/ust/hk/Lab1.java
+++ src/labs/comp3111/ust/hk/Lab1.java
@@ -3,5 +3,6 @@ package labs.comp3111.ust.hk;
 public class Lab1 {
 	public static void main(String[] args) {
 		System.out.println("Lab1: intro to Git and Github");
+		System.out.println("Congratulatons! You learnt the Git basics");
 	}
 }
